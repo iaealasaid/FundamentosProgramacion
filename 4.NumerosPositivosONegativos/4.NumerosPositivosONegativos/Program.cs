093eb5598b7cc7d528d79bfbe13c86128695ad90
Isabella <isabella.alzatem@upb.edu.co>
2026-02-25 03:48:27 -0500

    Numeritos

---
+++ 4.NumerosPositivosONegativos/4.NumerosPositivosONegativos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.NumerosPositivosONegativos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 0;
+            Console.WriteLine("Ingrese un número entero");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            if (Num >= 0)
+            {
+                Console.WriteLine("El número es positivo");
+            }
+            else
+            {
+                Console.WriteLine("El número es negativo");
+            }
+        }
+    }
+}
