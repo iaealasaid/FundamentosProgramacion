8b98b65d6462a4a17157bb629fa8e859fb475d32
Isabella <isabella.alzatem@upb.edu.co>
2026-03-04 09:27:06 -0500

    Taller

---
+++ 8.TallerProgramación/8.TallerProgramación/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.TallerProgramación
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taller programación
+
+            /*float numero1 = 0f;
+            float numero2 = 0f;
+            float numero3 = 0f;
+
+            Console.WriteLine("Ingrese números diferentes\n---");
+            Console.WriteLine("Ingrese el número 1");
+            numero1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número 2");
+            numero2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número 3");
+            numero3 = Single.Parse(Console.ReadLine());
+
+            if (numero1 != numero2 && numero1 != numero3 && numero2 != numero3)
+            {
+                if (numero2 >  numero3 && numero2 < numero1)
+                {
+                    Console.WriteLine($"El número mayor es: {numero1}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero1}, {numero2}, {numero3}");
+                }
+                else if (numero3 > numero2 && numero3 < numero1)
+                {
+                    Console.WriteLine($"El número mayor es: {numero1}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero1}, {numero3}, {numero2}");
+                }
+                else if (numero1 > numero3 && numero1 < numero2)
+                {
+                    Console.WriteLine($"El número mayor es: {numero2}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero2}, {numero1}, {numero3}");
+                }
+                else if (numero3 > numero1 && numero3 < numero2)
+                {
+                    Console.WriteLine($"El número mayor es: {numero2}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero2}, {numero3}, {numero1}");
+                }
+                else if (numero1 > numero2 && numero1 < numero3)
+                {
+                    Console.WriteLine($"El número mayor es: {numero3}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero3}, {numero1}, {numero2}");
+                }
+                else if (numero2 > numero1 && numero2 < numero3)
+                {
+                    Console.WriteLine($"El número mayor es: {numero3}");
+                    Console.WriteLine($"Los orden de los números de mayor a menor es: {numero3}, {numero2}, {numero1}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Debe ingresar números diferentes");
+            }*/
+
+
+            /*bool invencible = false;
+            int municion = 0;
+            string respuesta = "";
+            Random rnd = new Random();
+            municion = rnd.Next(1, 11);
+
+            Console.WriteLine($"Tienes {municion} de munición\n¿Su personaje se encuentra en estado invencible? (Ingrese 'si' o 'no')");
+            respuesta=Console.ReadLine();
+
+            if (respuesta == "si")
+            {
+                invencible = true;
+            }
+            Console.WriteLine(invencible == true && municion >= 1 ? "El personaje está disparando" : "");*/
+
+
+            float x1 = 0;
+            float y1 = 0;
+            float x2 = 0;
+            float y2 = 0;
+            float x3 = 0;
+            float y3 = 0;
+        }
+    }
+}
