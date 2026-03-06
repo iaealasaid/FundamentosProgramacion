f571aaa3f42682c6583eb20ac55c94fedd6666f0
Isabella <isabella.alzatem@upb.edu.co>
2026-03-06 00:45:42 -0500

    Taller

--- 8.TallerProgramación/8.TallerProgramación/Program.cs
+++ 8.TallerProgramación/8.TallerProgramación/Program.cs
@@ -79,12 +79,112 @@ namespace _8.TallerProgramación
             Console.WriteLine(invencible == true && municion >= 1 ? "El personaje está disparando" : "");*/
 
 
-            float x1 = 0;
+            /*float x1 = 0;
             float y1 = 0;
             float x2 = 0;
             float y2 = 0;
             float x3 = 0;
             float y3 = 0;
+
+            Console.WriteLine("Ingrese el valor de x para P1");
+            x1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de y para P1");
+            y1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de x para P2");
+            x2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de y para P2");
+            y2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de x para P3");
+            x3 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de y para P3");
+            y3 = Single.Parse(Console.ReadLine());
+
+            float dP1P2 = (float)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            float dP2P3 = (float)Math.Sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+            float dP1P3 = (float)Math.Sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+
+            bool triangulo = (dP1P2 + dP1P3 > dP2P3) && (dP1P2 + dP2P3 > dP1P3) && (dP2P3 + dP1P3 > dP1P2);
+            bool cond1 = (dP1P2 + dP1P3 > dP2P3);
+            bool cond2 = (dP1P2 + dP2P3 > dP1P3);
+            bool cond3 = (dP2P3 + dP1P3 > dP1P2);
+
+            Console.WriteLine($"Distancias: P1 a P2: {dP1P2} | P2 a P3: {dP2P3} | P1 a P3: {dP1P3}");
+            
+            if (!triangulo)
+            {
+                Console.WriteLine("No se puede contruir un triángulo");
+                if (!cond1)
+                {
+                    Console.WriteLine($"No cumple la condición: (P1 a P2) + (P1 a P3) > (P2 a P3) | {dP1P2} + {dP1P3} <= {dP2P3}");
+                }
+                if (!cond2)
+                {
+                    Console.WriteLine($"No cumple la condición: (P1 a P2) + (P2 a P3) > (P1 a P3) | {dP1P2} + {dP2P3} <= {dP1P3}");
+                }
+                if (!cond3)
+                {
+                    Console.WriteLine($"No cumple la condición: (P2 a P3) + (P1 a P3) > (P1 a P2) | {dP2P3} + {dP1P3} <= {dP1P2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Se puede construir un triángulo");
+            }*/
+
+
+            /*char direccion = ' ';
+
+            Console.WriteLine("El personaje se mueve hacia la izquierda o hacia la derecha, según la tecla que presione en el teclado\n¿Hacia que dirección se mueve el personaje? (Ingrese un carácter)");
+            direccion=Convert.ToChar(Console.ReadLine());
+
+            switch (direccion)
+            {
+                case 'a':
+                    Console.WriteLine("El personaje se mueve hacia la izquierda");
+                    break;
+                case 'd':
+                    Console.WriteLine("El personaje se mueve hacia la derecha");
+                    break;
+                default:
+                    Console.WriteLine("No me puedo mover en otra dirección");
+                    break;
+            }*/
+
+
+            int vidas;
+            Random rnd = new Random();
+            vidas = rnd.Next(0, 6);
+            char accion = ' ';
+
+            Console.WriteLine($"El jugador tiene {vidas} vida/s");
+
+            if (vidas > 0)
+            {
+                Console.WriteLine("¿Qué acción va a realizar? (c = disparar, x = hablar con la Rana, t = activar turbo, i = invencibilidad)");
+                accion=Convert.ToChar(Console.ReadLine());
+                switch (accion)
+                {
+                    case 'c':
+                        Console.WriteLine("El personaje está disparando");
+                        break;
+                    case 'x':
+                        Console.WriteLine("El personaje está hablando con la Rana");
+                        break;
+                    case 't':
+                        Console.WriteLine("El personaje está en modo Turbo");
+                        break;
+                    case 'i':
+                        Console.WriteLine("El personaje es invencible");
+                        break;
+                    default:
+                        Console.WriteLine("El jugador no realizo ninguna acción valida");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("El personaje no posee vidas, y no puede realizar ninguna acción");
+            }
         }
     }
 }
