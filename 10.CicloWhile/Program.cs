db82c0b6c6035a226589fcb823286cc946fb057d
Isabella <isabella.alzatem@upb.edu.co>
2026-04-08 03:13:07 -0500

    CicloWhile

--- 10.CicloWhile/Program.cs
+++ 10.CicloWhile/Program.cs
@@ -22,7 +22,8 @@ namespace _10.CicloWhile
             }
             Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");*/
 
-            int contador = 1;
+
+            /*int contador = 1;
             int acumulador = 1;
             int num = 0;
             Console.WriteLine("Ingrese el número");
@@ -33,7 +34,22 @@ namespace _10.CicloWhile
                 acumulador += contador;
                 contador++;
             }
-            Console.WriteLine($"El factorial de {num} es {acumulador}");
+            Console.WriteLine($"El factorial de {num} es {acumulador}");*/
+
+
+            int num = 0;
+            int acumulador = 0;
+
+            Console.WriteLine("Ingrese un número entero");
+            acumulador = Int32.Parse(Console.ReadLine());
+
+            while (num >= 0)
+            {
+                acumulador += num;
+                Console.WriteLine("Ingrese otro número entero");
+                num = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Ha ingresado un número negativo | La suma total de los números enteros positivos ingresados es {acumulador}");
         }
     }
 }
