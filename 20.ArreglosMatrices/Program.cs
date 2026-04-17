a8ffdac8566b3564fabcc8909979f56a7925e112
Isabella <isabella.alzatem@upb.edu.co>
2026-04-17 09:26:31 -0500

    ArreglosMatrices

---
+++ 20.ArreglosMatrices/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.ArreglosMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[,] numeros = new int[2, 3];
+            numeros[0, 0] = 15;
+            numeros[0, 1] = 15;
+            numeros[0, 2] = 23;
+            numeros[1, 0] = 36;
+            numeros[1, 1] = 22;
+            numeros[1, 2] = 100;
+            string[,] nombres =
+            {
+                    {"Ferney", "Ana"},
+                    {"Juan", "Carlos"},
+                    {"Yuli", "Esteban"}
+            };
+
+            Char[,] simbolos = new char[2, 5];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine($"Ingrese el símbolo para la P {i}, {j}");
+                    simbolos[i, j] = char.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < simbolos.GetLength(0); i++)
+            {
+                for(int j = 0; j < simbolos.GetLength(1); j++)
+                {
+                    Console.Write(simbolos[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }*/
+
+
+            /*int[,] matriz1 = new int[2, 3];
+            Console.WriteLine("------- Matriz 1 -------");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Ingrese el símbolo para la P {i}, {j}");
+                    matriz1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] matriz2 = new int[2, 3];
+            Console.WriteLine("\n------- Matriz 2 -------");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Ingrese el símbolo para la P {i}, {j}");
+                    matriz2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\n------- Matriz 1 -------");
+            for (int i = 0; i < matriz1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz1.GetLength(1); j++)
+                {
+                    Console.Write(matriz1[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n------- Matriz 2 -------");
+            for (int i = 0; i < matriz1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz1.GetLength(1); j++)
+                {
+                    Console.Write(matriz1[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }
+
+            int[,] suma = new int[2, 3];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    suma[i, j] = matriz1[i, j] + matriz2[i, j];
+                }
+            }
+
+            Console.WriteLine("\n--------- Suma ---------");
+            for (int i = 0; i < suma.GetLength(0); i++)
+            {
+                for (int j = 0; j < suma.GetLength(1); j++)
+                {
+                    Console.Write(suma[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }*/
+
+
+
+        }
+    }
+}
